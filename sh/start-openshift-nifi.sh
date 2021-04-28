878deab7ccbda195b0fefa44f590515933e09968
TassoneSE <80248640+TassoneSE@users.noreply.github.com>
2021-04-28 21:32:30 +0930

    remove access

--- sh/start-openshift-nifi.sh
+++ sh/start-openshift-nifi.sh
@@ -4,9 +4,5 @@
 
 [ ! -z $KUBERNETES_HEADLESS_SERVICE_NAME ] && HOSTNAME=$HOSTNAME.$KUBERNETES_HEADLESS_SERVICE_NAME
 
-chmod -R a+rwx /opt/nifi
-find /opt/nifi -type f -iname "*.sh" -exec chmod +x {} \;
-ls
-#RUN chmod +x /opt/nifi/scripts/toolkit.sh
 
 su ../scripts/start.sh
