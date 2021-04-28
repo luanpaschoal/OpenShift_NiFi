bec2048fa0ac83d56d579ed717145dc09fe42498
TassoneSE <80248640+TassoneSE@users.noreply.github.com>
2021-04-28 16:36:24 +0930

    crazy

--- sh/start-openshift-nifi.sh
+++ sh/start-openshift-nifi.sh
@@ -4,4 +4,9 @@
 
 [ ! -z $KUBERNETES_HEADLESS_SERVICE_NAME ] && HOSTNAME=$HOSTNAME.$KUBERNETES_HEADLESS_SERVICE_NAME
 
-../scripts/start.sh
+chmod -R a+rwx /opt/nifi
+find /opt/nifi -type f -iname "*.sh" -exec chmod +x {} \;
+ls
+#RUN chmod +x /opt/nifi/scripts/toolkit.sh
+
+su ../scripts/start.sh
