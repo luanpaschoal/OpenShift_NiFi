e0f959715f93399287e89fdff1a3025cfde38754
TassoneSE <80248640+TassoneSE@users.noreply.github.com>
2021-04-29 10:18:34 +1000

    ls

--- sh/start-openshift-nifi.sh
+++ sh/start-openshift-nifi.sh
@@ -1,9 +1,10 @@
 #!/bin/sh -e
 echo starting start-openshift-nifi.sh
 echo Path: $PWD
-[ ! -f conf/nifi.properties ] && cp -a nifi-1.11.4/conf .
+# [ ! -f conf/nifi.properties ] && cp -a nifi-1.11.4/conf .
 
 [ ! -z $KUBERNETES_HEADLESS_SERVICE_NAME ] && HOSTNAME=$HOSTNAME.$KUBERNETES_HEADLESS_SERVICE_NAME
+ls -la /opt/nifi
 
 echo kicking off start.sh
 ../scripts/start.sh
