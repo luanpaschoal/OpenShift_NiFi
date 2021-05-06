accbe0634639f015402f143de653953b652f0959
TassoneSE <80248640+TassoneSE@users.noreply.github.com>
2021-05-06 17:54:13 +1000

    Update start-openshift-nifi.sh

--- sh/start-openshift-nifi.sh
+++ sh/start-openshift-nifi.sh
@@ -2,19 +2,19 @@
 echo starting start-openshift-nifi.sh
 echo Path: $PWD
 
+#useradd -u 1000160005 poduser
+
+chown -R :1000160005 /opt/nifi/
+
 echo whoami:
 myuser=$(whoami)
 ehco I am: $myuser
-myid=$(id -g)
-echo my ID is: $myid
+myid=$(id)
+echo my IDs are: $myid
 
 echo Pre Nifi ID:
 id nifi
 
-usermod -a -G nifi $myuser
-
-myid=$(id)
-echo my ID are now: $myid
 
 #[ ! -f conf/nifi.properties ] && cp -a nifi-temp/conf .
 
