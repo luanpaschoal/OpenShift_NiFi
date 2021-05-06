d99ab8be961c6f08a18be2949cdc81def0eb7da7
TassoneSE <80248640+TassoneSE@users.noreply.github.com>
2021-05-06 13:34:30 +1000

    update GID and UID

--- sh/start-openshift-nifi.sh
+++ sh/start-openshift-nifi.sh
@@ -2,12 +2,22 @@
 echo starting start-openshift-nifi.sh
 echo Path: $PWD
 
+echo Pre Nifi ID:
+id nifi
+
+groupmod -g 1000160005
+
+usermod -u 1000160005 -g 1000160005 nifi
+echo Post Nifi ID:
+id nifi
+
 [ ! -f conf/nifi.properties ] && cp -a nifi-temp/conf .
 
 
 ls -alR /opt/nifi
 
-echo kicking off start.sh
+echo end
+#echo kicking off start.sh
 #su nifi
 
-../scripts/start.sh
+ ../scripts/start.sh
