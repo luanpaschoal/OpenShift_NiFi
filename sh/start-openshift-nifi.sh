2e9590a6da2a60d44e0a471f94c0d53fe2f60589
TassoneSE <80248640+TassoneSE@users.noreply.github.com>
2021-05-07 17:14:59 +1000

    Large update
    This should work

--- sh/start-openshift-nifi.sh
+++ sh/start-openshift-nifi.sh
@@ -1,13 +1,52 @@
 #!/bin/sh -e
 echo starting start-openshift-nifi.sh
 echo Path: $PWD
-echo Pre Nifi ID:
+
+echo "Copying over conf from temp if no conf/nifi.properties file"
+[ ! -f conf/nifi.properties ] && cp -a nifi-temp/conf .
+echo "conf copy done"
 
 whoami
 id
 
-echo "going to sleep now for 1 hour"
+echo "Creating group and adding users poduser and nifi"
+# create new user under Openshift accepted group UID. "$ oc describe project [Project-Name]"
+useradd -u 1000160001 poduser
+
+# create a new group under Openshift accepted group GID. "$ oc describe project [Project-Name]"
+groupadd -g 1000160001 nifipodusers
+
+# assign nif with a valid UID to run in Openshift
+#sudo usermod -u 1000160002 nifi
+
+# Add user nifi/poduser the group nifipodusers
+usermod -a -G nifipodusers nifi
+usermod -a -G nifipodusers poduser
+
+# Give group fill access to everything under /opt/nifi/
+chgrp -R nifipodusers /opt/nifi
+
+# clean up access so only root and users under group nifipodusers have access
+chmod -R 770 /opt/nifi
+
+
+#change to the new user
+su poduser
+
+# Just test to see who is running
+whoami
+id
+
+# List who owns dir's inside of nifi
+#ls -alR /opt/nifi
+ls -al /opt/nifi
+
+
+#echo sleeping for 1 hour
 sleep 1h
 
-echo kicking off start.sh
+
+echo Current Path: $PWD
+ls -al
+echo kicking off start.sh if its in the correct path
 #../scripts/start.sh
