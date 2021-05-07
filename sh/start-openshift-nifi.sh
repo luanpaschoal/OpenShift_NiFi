344ed723121147ac4d59e96aa6165ba667d438ea
TassoneSE <80248640+TassoneSE@users.noreply.github.com>
2021-05-07 10:14:06 +1000

    Update start-openshift-nifi.sh

--- sh/start-openshift-nifi.sh
+++ sh/start-openshift-nifi.sh
@@ -3,15 +3,17 @@ echo starting start-openshift-nifi.sh
 echo Path: $PWD
 echo Pre Nifi ID:
 
+echo whoami:
+myuser=$(whoami)
+ehco I am: $myuser
+myid=$(id)
+echo my IDs are: $myid
+
 echo "Creating new user and group for nifi user"
 # create new user under Openshift accepted group UID. "$ oc describe project [Project-Name]"
-sudo useradd -u 1000160010 poduser
+sudo useradd -u 1000160001 poduser
 # create a new group under Openshift accepted group GID. "$ oc describe project [Project-Name]"
 sudo groupadd -g 1000160001 nifipodusers
-
-# copy the temp conf dir over if there is no nifi.properties
-[ ! -f conf/nifi.properties ] && cp -a nifi-temp/conf .
-
 
 # Add the docker user nifi and the new user to the group nifipodusers
 sudo usermod -a -G nifipodusers nifi
@@ -19,7 +21,11 @@ sudo usermod -a -G nifipodusers poduser
 
 # Give group fill access to everything under /opt/nifi/
 sudo chgrp -R nifipodusers /opt/nifi
-sudo chmod -R 770 /opt/nifi
+#sudo chmod -R 770 /opt/nifi
+
+
+# copy the temp conf dir over if there is no nifi.properties
+[ ! -f conf/nifi.properties ] && cp -a nifi-temp/conf .
 
 #change to the new user
 sudo su poduser
@@ -31,6 +37,7 @@ ehco I am: $myuser
 myid=$(id)
 echo my IDs are: $myid
 
+ehco /n/n/n/nListing all the DIR:
 
 # List who owns dir's inside of nifi
 ls -alR /opt/nifi
