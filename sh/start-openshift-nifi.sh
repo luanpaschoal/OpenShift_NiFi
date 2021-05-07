e51879a52aa1464c4db46e68e83f049513deb4d4
TassoneSE <80248640+TassoneSE@users.noreply.github.com>
2021-05-07 16:25:15 +1000

    Update start-openshift-nifi.sh

--- sh/start-openshift-nifi.sh
+++ sh/start-openshift-nifi.sh
@@ -3,48 +3,11 @@ echo starting start-openshift-nifi.sh
 echo Path: $PWD
 echo Pre Nifi ID:
 
-echo whoami:
-myuser=$(whoami)
-ehco I am: $myuser
-myid=$(id)
-echo my IDs are: $myid
+whoami
+id
 
-echo "Creating new user and group for nifi user"
-# create new user under Openshift accepted group UID. "$ oc describe project [Project-Name]"
-sudo useradd -u 1000160001 poduser
-# create a new group under Openshift accepted group GID. "$ oc describe project [Project-Name]"
-sudo groupadd -g 1000160001 nifipodusers
-
-#sudo usermod -u 1000160002 nifi
-
-# Add the docker user nifi and the new user to the group nifipodusers
-sudo usermod -a -G nifipodusers nifi
-sudo usermod -a -G nifipodusers poduser
-
-# Give group fill access to everything under /opt/nifi/
-sudo chgrp -R :nifipodusers /opt/nifi
-sudo chmod -R 770 /opt/nifi
-
-
-# copy the temp conf dir over if there is no nifi.properties
-#[ ! -f conf/nifi.properties ] && cp -a nifi-temp/conf .
-
-#change to the new user
-sudo su - poduser
-
-# Just test to see who is running
-echo whoami:
-myuser=$(whoami)
-ehco I am: $myuser
-myid=$(id)
-echo my IDs are: $myid
-
-ehco "/n/n/n/nListing all the DIR:"
-
-# List who owns dir's inside of nifi
-ls -alR /opt/nifi
-
+echo "going to sleep now for 1 hour"
+sleep 1h
 
 echo kicking off start.sh
-sleep 360
 #../scripts/start.sh
