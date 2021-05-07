2f3731a601ae531940f7c5eb3207d59a9deacf3e
TassoneSE <80248640+TassoneSE@users.noreply.github.com>
2021-05-07 23:27:30 +1000

    Large update with documentation

--- sh/start-openshift-nifi.sh
+++ sh/start-openshift-nifi.sh
@@ -1,52 +1,71 @@
 #!/bin/sh -e
-echo starting start-openshift-nifi.sh
-echo Path: $PWD
+echo "####################################################################################"
+echo "## Welcomes to a custom bash script to allow this image to run on OpenShift       ##"
+echo "##                                                                                ##"
+echo "## Please see: https://www.openshift.com/blog/a-guide-to-openshift-and-uids       ##"
+echo "##             Under:TRADITIONAL APPLICATIONS AND UIDS                            ##"
+echo "####################################################################################\n\n"
 
-echo "Copying over conf from temp if no conf/nifi.properties file"
+echo "I am"
+whoami 
+echo "let me fix a few things for you..."
+
+echo "Copying conf DIR from nifi-temp/conf if nifi.properties is not in /opt/nifi/nifi-current nifi/conf DIR ..."
+# see https://issues.apache.org/jira/browse/NIFI-6484
+# see https://issues.apache.org/jira/browse/NIFI-6071
 [ ! -f conf/nifi.properties ] && cp -a nifi-temp/conf .
-echo "conf copy done"
+echo "Done\n"
 
-whoami
-id
+echo "Creating new user NiFi with UID 1000160002 ..."
+# create new user under OpenShift accepted group UID. "$ oc describe project [Project-Name]"
+usermod -u 1000160002 nifi
+echo "Done\n"
 
-echo "Creating group and adding users poduser and nifi"
-# create new user under Openshift accepted group UID. "$ oc describe project [Project-Name]"
-useradd -u 1000160001 poduser
-
+echo "Creating new group nifipodusers with GID 1000160001 ..."
 # create a new group under Openshift accepted group GID. "$ oc describe project [Project-Name]"
 groupadd -g 1000160001 nifipodusers
+echo "Done\n"
 
-# assign nif with a valid UID to run in Openshift
-usermod -u 1000160002 nifi
-
+echo "Adding user nifi to group nifipodusers ..."
 # Add user nifi/poduser the group nifipodusers
 usermod -a -G nifipodusers nifi
-#usermod -a -G nifipodusers poduser
+echo "Done\n"
 
+echo "Giving ownership of all DIR's under /opt/nifi to group nifipodusers ..."
 # Give group fill access to everything under /opt/nifi/
 chgrp -R nifipodusers /opt/nifi
+echo "Done\n"
 
+echo "Cleaning up /opt/nifi so only group members and root have access ..."
 # clean up access so only root and users under group nifipodusers have access
 chmod -R 770 /opt/nifi
+echo "Done\n"
 
+echo "Setting user nifi home/work directory as /opt/nifi/nifi-current ..."
+# set NiFi home directory
+usermod -d /opt/nifi/nifi-current nifi
+echo "Done\n"
 
+echo "Did I login as nifi? ..."
 #change to the new user
-su nifi
-
-# Just test to see who is running
-whoami
+su - nifi
+echo "I am:"
+whoami  
+echo "with id:"
 id
+echo "Done\n"
 
-# List who owns dir's inside of nifi
+# List who owns DIR's inside of nifi
 #ls -alR /opt/nifi
-ls -al /opt/nifi
+#ls -al
 
-
+# Enable sleep to inspect the pod
 #echo sleeping for 1 hour
-sleep 1h
+#sleep 1h
+#sleep 60
 
 
-echo Current Path: $PWD
+echo "current path ($PWD) should equal (/opt/nifi/nifi-current)\n\n"
 ls -al
-echo kicking off start.sh if its in the correct path
-#../scripts/start.sh
+echo "kicking off NiFi's original ENTRYPOINT start.sh as user nifi"
+../scripts/start.sh
